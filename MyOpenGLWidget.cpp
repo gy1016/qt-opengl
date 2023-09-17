52d7b3c436e5f3ca9b96124be0c3976ad5fefffd
lamb <1838940294@qq.com>
2023-09-17 11:21:01 +0800

    feat: 多个attribute绘制案例

--- MyOpenGLWidget.cpp
+++ MyOpenGLWidget.cpp
@@ -9,10 +9,10 @@ unsigned int shaderProgram;
 //	0.0f, 0.5f, 0.0f
 //};
 float vertices[] = {
-	0.5f, 0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-	-0.5f, -0.5f, 0.0f,
-	-0.5f, 0.5f, 0.0f,
+	0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+	0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+	-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+	-0.5f, 0.5f, 0.0f, 0.5f, 0.5f, 0.5f,
 };
 unsigned int indices[] = {
 	0, 1, 3,
@@ -69,9 +69,11 @@ void MyOpenGLWidget::initializeGL()
 
 
 	// 告知显卡如何解析缓冲里的属性值
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	// 开启VAO管理的第一个属性值
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	// 释放
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
--- polygon.frag
+++ polygon.frag
@@ -1,6 +1,7 @@
 #version 330 core
+in vec3 outColor;
 out vec4 FragColor;
 
 void main(){
-	FragColor = vec4(0.0, 0.5, 0.2, 1.0);
+	FragColor = vec4(outColor, 1.0);
 }
